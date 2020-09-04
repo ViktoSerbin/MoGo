b07434bb21c83a90fee1f98d48e991e68dab5916
ViktoSerbin <taur1721@gmail.com>
2020-09-05 00:28:42 +0300

    50% project

---
+++ app/js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    $('.search-btn').on('click', function () {
+        $('.header__search').slideToggle();
+    });
+
+
+    $('.top-slider__inner').slick({
+        arrows: false,
+        dots: true,
+    });
+
+    $('.services__item-title').on('click', function(){
+        $('.services__item').removeClass('services__item--active');
+        $(this).parent().addClass('services__item--active');
+    });
+
+    $('.services__comment-slider').slick({
+    });
+
+
+});
